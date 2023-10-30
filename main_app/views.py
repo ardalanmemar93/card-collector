93afe4dbf56514c75a9b3b6a8539727b0680ab50
ardalanmemar93 <ardalanmemar@gmail.com>
2023-10-30 14:01:51 -0700

    card collector project done for now

---
+++ main_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+cards = [
+    {'name': "Tetsuo's Dream", 'from': "Akira", 'description': "Tetsuo dreams about being in a lab and getting tested on.", 'year': 1987},
+    {'name': "Kiyoko's Dream", 'from': "Akira", 'description': "She dreams about tetsuo destroying the city", 'year': 1987},
+    {'name': "Neo Tokyo", 'from': "Akira", 'description': "Neo Tokyo was built by 2019, the city pulses with light, sound, motion and apparentprosperity.", 'year': 1987}
+]
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cards_index(request):
+    return render(request, 'cards/index.html', {
+        'cards': cards
+    })
