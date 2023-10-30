5bec4054332262e190859afe65e262f8a1ea517f
ardalanmemar93 <ardalanmemar@gmail.com>
2023-10-30 14:27:05 -0700

    added shadows to cards

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 cards = [
     {'name': "Tetsuo's Dream", 'from': "Akira", 'description': "Tetsuo dreams about being in a lab and getting tested on.", 'year': 1987},
     {'name': "Kiyoko's Dream", 'from': "Akira", 'description': "She dreams about tetsuo destroying the city", 'year': 1987},
-    {'name': "Neo Tokyo", 'from': "Akira", 'description': "Neo Tokyo was built by 2019, the city pulses with light, sound, motion and apparentprosperity.", 'year': 1987}
+    {'name': "Neo Tokyo", 'from': "Akira", 'description': "Neo Tokyo was built by 2019, the city pulses with light, sound, motion and apparentprosperity.", 'year': 1987},
 ]
 
 # Create your views here.
