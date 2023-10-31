261c1db97a3092e5d88e9c78e0ed60ac25a53245
ardalanmemar93 <ardalanmemar@gmail.com>
2023-10-31 14:59:58 -0700

    crads lab part 2 baby

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-
-
-cards = [
-    {'name': "Tetsuo's Dream",  'number': 22, 'from': "Akira", 'description': "Tetsuo dreams about being in a lab and getting tested on.", 'year': 1987},
-    {'name': "Kiyoko's Dream", 'number': 23,  'from': "Akira", 'description': "She dreams about tetsuo destroying the city", 'year': 1987},
-    {'name': "Neo Tokyo", 'number': 2,  'from': "Akira", 'description': "Neo Tokyo was built by 2019, the city pulses with light, sound, motion and apparentprosperity.", 'year': 1987},
-    {'name': "Faithful Kaori",  'number': 70, 'from': "Akira", 'description': "Kaori finally reaches the remains of the olympic stadium", 'year': 1987},
-    {'name': "The Calm Before",  'number': 69, 'from': "Akira", 'description': "The Colonel watches the debris from SOL rain like shooting stars on Neo Tokyo", 'year': 1987},
-    {'name': "Akira Reborn", 'number': 76, 'from': "Akira", 'description': "A small figure appears in the light, translucent for a moment, then solid.", 'year': 1987},
-]
+from .models import Card
 
 # Create your views here.
 def home(request):
@@ -18,6 +9,11 @@ def about(request):
     return render(request, 'about.html')
 
 def cards_index(request):
+    cards = Card.objects.all()
     return render(request, 'cards/index.html', {
         'cards': cards
     })
+    
+def cards_detail(request, card_id):
+    card = Card.objects.get(id=card_id)
+    return render(request, 'cards/detail.html', { 'card': card })
